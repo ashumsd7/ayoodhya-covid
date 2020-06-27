4ed76b4c07d650d57c593fd1d59c7bf2d6adcaac
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-06-27 22:50:13 +0530

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -2,55 +2,55 @@ new Vue({
     el:"#app",
     data() {
         return {
-            data:'26 जून 2020',
+            data:'27 जून 2020',
             site_title: "अयोध्या",
             today_recvd_reports_txt:' प्राप्त रिपोर्ट',
-            today_recvd_reports:'423',
+            today_recvd_reports:'536',
             
             today_positive_reports_text:' पॉजिटिव केस',
-            today_positive_reports:'14',
+            today_positive_reports:'3',
 
             today_negative_reports_txt:' नेगेटिव केस',
-            today_negative_reports:'409',
+            today_negative_reports:'533',
 
             today_recovered_reports_txt:' ठीक हुए',
-            today_recovered_reports:'5',
+            today_recovered_reports:'2',
 
             today_samples_taken_report_txt:' लिए गए सैंपल',
-            today_samples_taken_reports:'344',
+            today_samples_taken_reports:'362',
 
             total_positive_txt:'कुल पॉजिटिव मामले',
-            total_positive:'263',
+            total_positive:'266',
 
             total_active_txt:'कुल सक्रीय',
-            total_active:'81',
+            total_active:'82',
 
             total_recovered_txt:'कुल ठीक',
-            total_recovered:'178',
+            total_recovered:'180',
 
             total_deaths_txt:'कुल मृतक',
             total_deaths:'4',
 
             total_sent_samples_txt:'कुल भेजे गए सैंपल रिपोर्ट्स',
-            total_sent_samples:'8396',
+            total_sent_samples:'8758',
 
             total_recvd_sample_txt:'कुल प्राप्त सैंपल रिपोर्ट्स',
-            total_recvd_sample:'7641',
+            total_recvd_sample:'8177',
 
             total_not_rcvd_samples_txt:'कुल अप्राप्त सैंपल रिपोर्ट्स',
-            total_not_rcvd_samples:'755',
+            total_not_rcvd_samples:'581',
 
             total_clusters:'31',
             total_zones:'62',
 
             sadar_clusters:'12',
-            sadar_zones:'20',
+            sadar_zones:'21',
 
             milkipur_clusters:'7',
             milkipur_zones:'14',
 
             bikapur_clusters:'5',
-            bikapur_zones:'4',
+            bikapur_zones:'3',
 
             sohawal_clusters:'5',
             sohawal_zones:'8',
