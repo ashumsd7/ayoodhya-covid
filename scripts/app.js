45987deb2ce85d7064bbe85734e0a206e0f0bb41
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-06-30 04:51:01 +0530

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -2,52 +2,52 @@ new Vue({
     el:"#app",
     data() {
         return {
-            data:'28 जून 2020',
+            data:'29 जून 2020',
             site_title: "अयोध्या कोरोना अपडेट्स ",
             today_recvd_reports_txt:' प्राप्त रिपोर्ट',
-            today_recvd_reports:'191',
+            today_recvd_reports:'693',
             
             today_positive_reports_text:' पॉजिटिव केस',
-            today_positive_reports:'6',
+            today_positive_reports:'04',
 
             today_negative_reports_txt:' नेगेटिव केस',
-            today_negative_reports:'185',
+            today_negative_reports:'689',
 
             today_recovered_reports_txt:' ठीक हुए',
-            today_recovered_reports:'10',
+            today_recovered_reports:'04',
 
             today_samples_taken_report_txt:' लिए गए सैंपल',
-            today_samples_taken_reports:'333',
+            today_samples_taken_reports:'340',
 
             total_positive_txt:'कुल पॉजिटिव मामले',
-            total_positive:'272',
+            total_positive:'276',
 
             total_active_txt:'कुल सक्रीय',
             total_active:'78',
 
             total_recovered_txt:'कुल ठीक',
-            total_recovered:'190',
+            total_recovered:'194',
 
             total_deaths_txt:'कुल मृतक',
             total_deaths:'4',
 
             total_sent_samples_txt:'कुल भेजे गए सैंपल रिपोर्ट्स',
-            total_sent_samples:'9091',
+            total_sent_samples:'9431',
 
             total_recvd_sample_txt:'कुल प्राप्त सैंपल रिपोर्ट्स',
-            total_recvd_sample:'8368',
+            total_recvd_sample:'9061',
 
             total_not_rcvd_samples_txt:'कुल अप्राप्त सैंपल रिपोर्ट्स',
-            total_not_rcvd_samples:'723',
+            total_not_rcvd_samples:'370',
 
             total_clusters:'32',
-            total_zones:'61',
+            total_zones:'63',
 
             sadar_clusters:'12',
             sadar_zones:'22',
 
             milkipur_clusters:'7',
-            milkipur_zones:'15',
+            milkipur_zones:'16',
 
             bikapur_clusters:'5',
             bikapur_zones:'3',
@@ -56,7 +56,7 @@ new Vue({
             sohawal_zones:'6',
 
             rudauli_clusters:'3',
-           rudauli_zones:'15',
+           rudauli_zones:'16',
 
 
            masodha_total_beds:'54',
@@ -64,12 +64,12 @@ new Vue({
            masodha_free_beds:'54',
 
            dashrath_total_beds:'50',
-           dashrath_used_beds:'10',
-           dashrath_free_beds:'40',
+           dashrath_used_beds:'19',
+           dashrath_free_beds:'31',
 
            luvkush_total_beds:'300',
-           luvkush_used_beds:'159',
-           luvkush_free_beds:'141',
+           luvkush_used_beds:'152',
+           luvkush_free_beds:'148',
 
            L1masodha_toatal_beds:'32',
            L1masodha_used_beds:'0',
@@ -80,8 +80,8 @@ new Vue({
            L1jhun_free_beds:'35',
 
            L1dashrath_toatal_beds:'200',
-           L1dashrath_used_beds:'36',
-           L1dashrath_free_beds:'164',
+           L1dashrath_used_beds:'33',
+           L1dashrath_free_beds:'167',
 
            L1kumarganj_toatal_beds:'200',
            L1kumarganj_used_beds:'0',
