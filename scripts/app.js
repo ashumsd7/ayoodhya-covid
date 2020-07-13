1b86996c80a0d3dd5977be295543ea1c96856457
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-07-13 23:26:09 +0530

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -5,42 +5,42 @@
             data:'13 जुलाई 2020',
             site_title: "अयोध्या कोरोना अपडेट्स ",
             today_recvd_reports_txt:' प्राप्त रिपोर्ट',
-            today_recvd_reports:'392',
+            today_recvd_reports:'853',
             
             today_positive_reports_text:' पॉजिटिव केस',
-            today_positive_reports:'6',
+            today_positive_reports:'15',
 
             today_negative_reports_txt:' नेगेटिव केस',
-            today_negative_reports:'386',
+            today_negative_reports:'838',
 
             today_recovered_reports_txt:' ठीक हुए',
-            today_recovered_reports:'4',
+            today_recovered_reports:'17',
 
             today_samples_taken_report_txt:' लिए गए सैंपल',
-            today_samples_taken_reports:'720',
+            today_samples_taken_reports:'672',
 
             total_positive_txt:'कुल पॉजिटिव मामले',
-            total_positive:'445',
+            total_positive:'460',
 
             total_active_txt:'कुल सक्रीय',
-            total_active:'155',
+            total_active:'153',
 
             total_recovered_txt:'कुल ठीक',
-            total_recovered:'285',
+            total_recovered:'302',
 
             total_deaths_txt:'कुल मृतक',
             total_deaths:'5',
 
             total_sent_samples_txt:'कुल भेजे गए सैंपल रिपोर्ट्स',
-            total_sent_samples:'16667',
+            total_sent_samples:'17339',
 
             total_recvd_sample_txt:'कुल प्राप्त सैंपल रिपोर्ट्स',
-            total_recvd_sample:'15171',
+            total_recvd_sample:'16026',
 
             total_not_rcvd_samples_txt:'कुल अप्राप्त सैंपल रिपोर्ट्स',
-            total_not_rcvd_samples:'1496',
+            total_not_rcvd_samples:'1313',
 
-            total_clusters:'42',
+            total_clusters:'45',
             total_zones:'111',
 
             sadar_clusters:'14',
@@ -64,20 +64,20 @@
            masodha_free_beds:'54',
 
            dashrath_total_beds:'50',
-           dashrath_used_beds:'30',
-           dashrath_free_beds:'20',
+           dashrath_used_beds:'40',
+           dashrath_free_beds:'10',
 
            luvkush_total_beds:'300',
-           luvkush_used_beds:'192',
-           luvkush_free_beds:'108',
+           luvkush_used_beds:'118',
+           luvkush_free_beds:'182',
 
            L1masodha_toatal_beds:'32',
            L1masodha_used_beds:'0',
            L1masodha_free_beds:'32',
 
            L1jhun_toatal_beds:'80',
-           L1jhun_used_beds:'48',
-           L1jhun_free_beds:'32',
+           L1jhun_used_beds:'42',
+           L1jhun_free_beds:'38',
 
            L1dashrath_toatal_beds:'200',
            L1dashrath_used_beds:'107',
